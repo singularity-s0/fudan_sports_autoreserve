cb778d70b891923e43de343c942e9d04644a9ada
Wang Zhongyao <47234030+wzy461143268@users.noreply.github.com>
2023-08-21 09:50:47 +0800

    Update main.py
    1、自动确定预定场地日期
    2、并发，一次预定多个时间段

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 import apis
 import logs
+import concurrent.futures
 
 SERVICE_CATEGORY = "2c9c486e4f821a19014f82381feb0001"  # This is the category ID for "Sports Reservation". It usually doesn't change.
 
 # Fill in these data
-USER_ID = "YOUR_USER_ID"
-USER_PASSWORD = "YOUR_USER_PASSWORD"
-CAMPUS_NAME = "邯郸校区"
+USER_ID = ""
+USER_PASSWORD = ""
+CAMPUS_NAME = "校区"
 SPORT_NAME = "羽毛球"
-SPORT_LOCATION = "正大体育馆羽毛球(标场)"
-DATE = "2022-03-06"
-TIME = "10:00"
+SPORT_LOCATION = "XXXX场"
+from datetime import datetime, timedelta
+
+today = datetime.today()
+two_days_later = today + timedelta(days=2)
+DATE = two_days_later.strftime("%Y-%m-%d") #自动延后两天
+TIMES = ["08:00","09:00"] #可以一次预定多个时间段
 
 # Optional data
-EMAILS = ["YOUR_EMAIL"]  # Receive error notifications by email
-YOUR_EMAIL = "YOUR_EMAIL"  # Account to send email from
-EMAIL_PASSWORD = "YOUR_EMAIL_PASSWORD"  # Password for the email account
+EMAILS = []  # Receive error notifications by email
+YOUR_EMAIL = None  # Account to send email from
+EMAIL_PASSWORD = None  # Password for the email account
 
 
-if __name__ == '__main__':
+
+def main_request(TIME: str):
     try:
+        print(DATE)
         logged_in_session = apis.login(USER_ID, USER_PASSWORD)
         campus_id, sport_id = apis.load_sports_and_campus_id(logged_in_session, SERVICE_CATEGORY, CAMPUS_NAME, SPORT_NAME)
         service_id = apis.get_service_id(logged_in_session, SERVICE_CATEGORY, campus_id, sport_id, SPORT_LOCATION)
@@ -37,3 +44,7 @@ if __name__ == '__main__':
                 connection.sendmail(YOUR_EMAIL, EMAILS, message)
             finally:
                 connection.quit()
+
+if __name__ == '__main__':
+    with concurrent.futures.ThreadPoolExecutor(max_workers=len(TIMES)) as executor:
+        executor.map(main_request, TIMES)
